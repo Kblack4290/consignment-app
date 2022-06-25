d8394f9d59369d1b06e5cc97325032d7f31564f0
Keith Laptop <Keithblack4290@gmail.com>
2022-06-25 17:03:03 -0600

    setting up form template

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [AppComponent, ItemListComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, ItemListComponent, FormComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
